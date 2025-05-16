b30c2253ebaf41b779060fe494356760476614b9
XG <xinggao27@gmail.com>
2025-05-16 12:48:52 -0500

    may 2025 update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 window.onload = function() {
                 
-    showPapers();
+    showPapers(PAPERS,'#publication-list');
+    showPapers(MANUSCRIPTS,'#manuscript-list');
 
 };
 
-function showPapers(){
-    var papers = PAPERS.replaceAll('\n','').replaceAll('{-}','-').split("@");
+function showPapers(paperStr,elementID){
+    var papers = paperStr.replaceAll('\n','').replaceAll('{-}','-').split("@");
     var paperList = [];
     papers.forEach(function(p){
         if(p!==''){
@@ -13,15 +14,16 @@ function showPapers(){
         }
     });
     paperList.forEach(function(paper){
-        var li = $('<li class="paper-li"/>').appendTo('#publication-list');
+        var li = $('<li class="paper-li"/>').appendTo(elementID);
+
         var titleElement = $("<div/>",{
             "class" : "paper-title"
         }).appendTo(li);
         if (paper.url){
-            $("<a/>",{target:"_blank", href:paper.url,text:paper.title+"."}).appendTo(titleElement);
+            $("<a/>",{target:"_blank", href:paper.url,text:paper.title+" "}).appendTo(titleElement);
         }
         else{
-            $("<span/>",{text:paper.title+"."}).appendTo(titleElement);
+            $("<span/>",{text:paper.title+" "}).appendTo(titleElement);
         }
         if (paper.other.length>0){
             paper.other.forEach(function(o,i){
@@ -44,10 +46,8 @@ function showPapers(){
         }).appendTo(li);
         paper.author.forEach(function(a,i){
             if (i!==0){
-                a = ", "+a;
-            }
-            if(i==paper.author.length-1){
-                a = a+".";
+                // a = ", "+a;
+                $("<span/>",{text:', '}).appendTo(authorElement);
             }
             if (a.indexOf("Xing Gao")>=0){
                 $("<span/>",{text:a,class:"author-me"}).appendTo(authorElement);
@@ -55,12 +55,23 @@ function showPapers(){
             else{
                 $("<span/>",{text:a,class:"author-not-me"}).appendTo(authorElement);
             }
+            if(i==paper.author.length-1){
+                // a = a+".";
+                $("<span/>",{text:'.'}).appendTo(authorElement);
+            }
         });
+
         var infoElement = $("<div/>",{
             "class" : "paper-author"
         }).appendTo(li);
-        $("<span/>",{text:"In "+paper.pub,class:"publisher "+paper.pub}).appendTo(infoElement);
-        $("<span/>",{text:", "+paper.year+".",class:""}).appendTo(infoElement);
+        // $("<span/>",{text:"In "+paper.pub,class:"publisher "+paper.pub}).appendTo(infoElement);
+        $("<span/>",{text:"In "+paper.pub,class:"publisher "}).appendTo(infoElement);
+        if (paper.year){
+            $("<span/>",{text:", "+paper.year+".",class:""}).appendTo(infoElement);
+        }
+        else{
+            $("<span/>",{text:".",class:""}).appendTo(infoElement);
+        }
         
     });
     console.log(paperList);
@@ -104,6 +115,9 @@ function parsePublisher(p){
     else if (p.entryType=="article"){
         pub = p.entryTags.journal;
     }
+    else if (p.entryType=="manu"){
+        pub = p.entryTags.journal;
+    }
     
     return pub;
 }
@@ -126,6 +140,14 @@ function parseAuthor(authorStr){
 
 
 var PAPERS = `
+@inproceedings{gao2025non,
+  title={Non-Center-Based Clustering Under Bilu-Linial Stability},
+  author={Gao, Xing and Reyzin, Lev},
+  year={2025},
+  booktitle={2025 IEEE International Symposium on Information Theory (ISIT)}, 
+  organization={IEEE},
+  url={./doc/2025/a186-gao_finalmanuscript.pdf}
+}
 @inproceedings{gao2023robust,
   title={Robust Matrix Sensing in the Semi-Random Model},
   author={Gao, Xing and Cheng, Yu},
@@ -135,7 +157,7 @@ var PAPERS = `
   url={./doc/2023/7698_robust_matrix_sensing_in_the_s.pdf},
   other={poster={./doc/2023/7698_robust_matrix_sensing_in_the_s_poster.pdf},video={https://neurips.cc/virtual/2023/poster/70501}}
 }
-@inproceedings{10313487,
+@inproceedings{gao2023unified,
   author={Gao, Xing and Maranzatto, Thomas and Reyzin, Lev},
   booktitle={2023 59th Annual Allerton Conference on Communication, Control, and Computing (Allerton)}, 
   title={A Unified Analysis of Dynamic Interactive Learning}, 
@@ -153,6 +175,25 @@ var PAPERS = `
   year={2022},
   organization={IEEE},
   url={./doc/2022/An_Interactive_Search_Game_with_Two_Agents.pdf}
+}
+`
+
+var MANUSCRIPTS = `
+@article{attias2025learning,
+  title={Learning-Augmented Algorithms for Boolean Satisfiability},
+  author={Attias, Idan and Gao, Xing and Reyzin, Lev},
+  url={https://arxiv.org/abs/2505.06146},
+  journal = {submission}
+}
+@article{gao2025Semi,
+ title={Semi-Random Noisy and One-Bit Matrix Completion With Nonconvex Primal-Dual Framework},
+ author={Gao, Xing and Chen, Binhao and Cheng, Yu},
+ journal = {submission}
+}
+@article{gao2025Margin,
+  title={Margin-Based Boosting for Partial Interpretability},
+  author={Gao, Xing and Reyzin, Lev and Wu, Shuo},
+  journal = {submission}
 }
 `
 
